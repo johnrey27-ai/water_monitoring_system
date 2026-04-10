0517128fb15b78c3c591a00e206e383430ab400e
johnrey27-ai <nillamajohnrey1027@gmail.com>
2026-04-10 11:21:06 +0800

    Updated views and urls file

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,4 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("Water Monitoring System is Running!")
+def dashboard(request):
+    data = {
+        'temperature': 28,
+        'ph': 7.2,
+        'status': 'Safe',
+        'records': [
+            {'temperature': 27, 'ph': 7.0, 'status': 'Safe'},
+            {'temperature': 30, 'ph': 6.5, 'status': 'Warning'},
+            {'temperature': 32, 'ph': 5.8, 'status': 'Danger'},
+        ]
+    }
+    return render(request, 'monitoring/dashboard.html', data)
